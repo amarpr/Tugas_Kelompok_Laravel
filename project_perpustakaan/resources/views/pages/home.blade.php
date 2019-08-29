314bc8bb0e137b8371870e40ff97d6491c6b75b5
sundara23 <sundara.career@gmail.com>
2019-08-29 17:08:07 +0700

    menambah komentar setiap bagian header konten dan footer

--- project_perpustakaan/resources/views/pages/home.blade.php
+++ project_perpustakaan/resources/views/pages/home.blade.php
@@ -1,19 +1,23 @@
 @extend('layouts.user')
 @section('title', 'Halaman Utama')
 
+<!-- bagian Header Disini -->
 @section('header')
 
 
 @endsection
+<!-- end bagian header  -->
 
-
+<!-- bagian content disini  -->
 @section('content')
 
 
 @endsection
+<!-- end bagian content  -->
 
-
+<!-- bagian footer disini  -->
 @section('footer')
 
 
 @endsection
+<!-- end bagian footer  -->
