3d19c285bd1e218153403d4d5e7b2756fbcb421e
rizal <mrizalmaulana000@yahoo.com>
2019-08-29 21:56:27 +0700

    add dummy content home

--- project_perpustakaan/resources/views/pages/home.blade.php
+++ project_perpustakaan/resources/views/pages/home.blade.php
@@ -1,10 +1,83 @@
 @extends('layouts.user')
+
 @section('title', 'Halaman Utama')
 
-
-
-<!-- bagian content disini  -->
 @section('content')
-alskjf;lksjd;lksjf;lkjlfksjlkfjal
-
+    @php
+        $datas = [
+            [
+                "image" => "ini untuk photo",
+                "name" => "ini untuk nama",
+                "desc" => "ini untuk keterangan",
+                "other" => "lain-lain",
+                "btn" => "deatil" 
+            ],
+            [
+                "image" => "ini untuk photo",
+                "name" => "ini untuk nama",
+                "desc" => "ini untuk keterangan",
+                "other" => "lain-lain",
+                "btn" => "deatil" 
+            ],
+            [
+                "image" => "ini untuk photo",
+                "name" => "ini untuk nama",
+                "desc" => "ini untuk keterangan",
+                "other" => "lain-lain",
+                "btn" => "deatil" 
+            ],
+            [
+                "image" => "ini untuk photo",
+                "name" => "ini untuk nama",
+                "desc" => "ini untuk keterangan",
+                "other" => "lain-lain",
+                "btn" => "deatil" 
+            ],
+            [
+                "image" => "ini untuk photo",
+                "name" => "ini untuk nama",
+                "desc" => "ini untuk keterangan",
+                "other" => "lain-lain",
+                "btn" => "deatil" 
+            ],
+            [
+                "image" => "ini untuk photo",
+                "name" => "ini untuk nama",
+                "desc" => "ini untuk keterangan",
+                "other" => "lain-lain",
+                "btn" => "deatil" 
+            ],
+            [
+                "image" => "ini untuk photo",
+                "name" => "ini untuk nama",
+                "desc" => "ini untuk keterangan",
+                "other" => "lain-lain",
+                "btn" => "deatil" 
+            ],
+            [
+                "image" => "ini untuk photo",
+                "name" => "ini untuk nama",
+                "desc" => "ini untuk keterangan",
+                "other" => "lain-lain",
+                "btn" => "deatil" 
+            ],
+        ];
+    @endphp
+    <div class="container">
+        <div class="row">
+        @foreach ($datas as $data)
+            <div class="col-xl-3 col-lg-3 col-md-6 col-12 my-3">
+                <div class="card">
+                <img src="{{ asset('/img/deception-point.jpg') }}" class="card-img-top" alt="{{ $data['image'] }}">
+                <div class="card-body">
+                    <h5 class="card-title">{{ $data['name'] }}</h5>
+                    <p class="card-text">{{ $data['desc'] }}</p>
+                    <p class="card-text">{{ $data['other'] }}</p>
+                    <button class="btn btn-primary"> {{ $data['btn'] }} </button>
+                </div>
+                </div>
+            </div>
+        @endforeach
+        </div>
+    </div>
 @endsection
