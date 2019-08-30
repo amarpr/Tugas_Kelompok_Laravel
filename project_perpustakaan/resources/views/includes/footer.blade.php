68d1a1f492852802a4e689a7ff666197bc9ce5b3
sundara23 <sundara.career@gmail.com>
2019-08-30 15:14:20 +0700

    no message

--- project_perpustakaan/resources/views/includes/footer.blade.php
+++ project_perpustakaan/resources/views/includes/footer.blade.php
@@ -1,28 +1,27 @@
 <section id="footer" class="mt-3">
+    <div class="container">
+        <div class="row py-3">
+            <div class="col">
+                <h4 class="text-primary">Reach Us</h4>
+                <li>
+                    <p>Alamat : Jl.Percetakan Negara 9 A No. 75 C ( samping Pos Polisi ), Jakarta Pusat</p>
+                </li>
+                <li>
+                    <p>Telepon : 0815 8583 7892, 021-4148951</p>
+                </li>
+                <li>
+                    <p>Email : Ar_rasyid.info@gmail.com </p>
+                </li>
+                <img src="/img/logofb.png" alt="">
+                <img src="/img/logotwitter.png" alt="">
+                <img src="/img/logoyt.png" alt="">
+                <img src="/img/logog+.png" alt="">
+            </div>
+        </div>
+    </div>
+    <div class="row pt-3" id="copyrightcontent">
         <div class="container">
-            <div class="row py-3">
-                <div class="col">
-                    <h4 class="text-primary">Reach Us</h4>
-                    <li>
-                        <p>Alamat : Jl.Percetakan Negara 9 A No. 75 C ( samping Pos Polisi ), Jakarta Pusat</p>
-                    </li>
-                    <li>
-                        <p>Telepon : 0815 8583 7892, 021-4148951</p>
-                    </li>
-                    <li>
-                        <p>Email : Ar_rasyid.info@gmail.com </p>
-                    </li>
-                    <img src="/img/logofb.png" alt="">
-                    <img src="/img/logotwitter.png" alt="">
-                    <img src="/img/logoyt.png" alt="">
-                    <img src="/img/logog+.png" alt="">
-                </div>
-            </div>
+            <p>Copyright © 2019 http://Ar_rasyid.com/ - All Rights Reserved. <a href="">Privacy Policy</a> | <a href="">Terms of Service</a> </p>
         </div>
-        <div class="row pt-3" id="copyrightcontent">
-            <div class="container">
-                <p>Copyright © 2019 http://Ar_rasyid.com/ - All Rights Reserved. <a href="">Privacy Policy</a> | <a
-                        href="">Terms of Service</a> </p>
-            </div>
-        </div>
-    </section>
+    </div>
+</section>
