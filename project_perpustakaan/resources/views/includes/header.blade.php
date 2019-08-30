4a7d656695b71e7c90c98f9eb960e8872d8d7fd3
antare74 <antare74@localhost.localdomain>
2019-08-30 08:13:59 +0700

    header koreksi

---
+++ project_perpustakaan/resources/views/includes/header.blade.php
@@ -0,0 +1,53 @@
+
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+
+<!-- konten header -->
+<div class="container-fluid">
+    <div class="row mx-2 my-2">
+        <div class="col-md-5">
+            <h2>PERPUSTAKAAN AR-RASYID</h2>
+            <marquee behavior="#" direction="#"><p>Лорем ипсум долор сит амет, иусто омиттам волуптатум дуо ад, сед пертинах сенсерит ад. Дуо антиопам партиендо ехплицари ан, аудире цоммодо цонцлусионемяуе вим ан. Делецтус десерунт ан нам. Ин еяуидем маиорум при, ест ут яуодси маиорум, про еу алияуандо сентентиае диспутатиони.</p>
+            </marquee>
+        </div>
+        <div class="col-md-7">
+           <div class="sign mt-5 float-right mr-5">
+            <button type="button" class="btn btn-sm btn-outline-primary">Signin</button>
+            <button type="button" class="btn btn-sm btn-primary">Signup</button>
+            </div> 
+        </div>
+    </div>
+<!-- tutup header  -->
+<!-- navbar -->
+    <nav class="navbar navbar-expand-lg navbar-light bg-danger px-5">
+        <a class="navbar-brand" href="#"><img src="https://pngimage.net/wp-content/uploads/2018/05/buku-logo-png-4.png" style="width:4rem" alt="gambar-logo-perpustakaan"></a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav mr-auto">
+                <li class="nav-item active">
+                  <a class="nav-link" href="#">Beranda <span class="sr-only">(current)</span></a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" href="#">Profil</a>
+                </li>
+                <li class="nav-item dropdown">
+                  <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                    Kategori
+                  </a>
+                  <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                    <a class="dropdown-item" href="#">Buku Programmer</a>
+                    <a class="dropdown-item" href="#">Majalah</a>
+                    <a class="dropdown-item" href="#">Komik</a>
+                  </div>
+                </li>
+            </ul>
+        <form class="form-inline my-2 my-lg-0">
+            <input class="form mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+            <button class="btn btn-sm btn-success my-2 my-sm-0" type="submit">Search</button>
+        </form>
+        </div>
+    </nav>
+<!-- tutup navbar -->
+</div>
