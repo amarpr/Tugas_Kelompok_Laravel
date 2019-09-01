58526e44ca91fef52221937cc2d4a264ffb83306
29music <hendra@localhost.localdomain>
2019-09-02 00:54:23 +0700

    form admin *kurang table admin

--- project_perpustakaan/routes/web.php
+++ project_perpustakaan/routes/web.php
@@ -26,3 +26,7 @@ Route::get('/comic', function () {
 Route::get('/majalah', function () {
     return view('pages.majalah');
 });
+
+Route::get('/admin', function () {
+    return view('admin.a_master');
+});
